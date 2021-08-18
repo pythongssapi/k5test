9f4e490d93c92e1b92d23ff82fb605c297515e3f
Robbie Harwood <rharwood@redhat.com>
2021-08-18 08:47:29 -0400

    Prepare for v0.10.0 release
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='k5test',
-    version='0.9.2',
+    version='0.10.0',
     author='The Python GSSAPI Team',
     author_email='sross@redhat.com',
     packages=['k5test'],
