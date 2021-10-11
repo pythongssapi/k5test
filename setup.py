86a1dd64199f013d30318743793f12ebf36c20b8
Jordan Borean <jborean93@gmail.com>
2021-10-11 10:33:55 +1000

    Add basic sanity checks and CI
    Signed-off-by: Jordan Borean <jborean93@gmail.com>

--- setup.py
+++ setup.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-
 setup(
-    name='k5test',
-    version='0.10.0',
-    author='The Python GSSAPI Team',
-    author_email='sross@redhat.com',
-    packages=['k5test'],
-    description='A library for testing Python applications in '
-                'self-contained Kerberos 5 environments',
-    long_description=open('README.md').read(),
-    license='LICENSE.txt',
+    name="k5test",
+    version="0.10.1",
+    author="The Python GSSAPI Team",
+    author_email="sross@redhat.com",
+    packages=["k5test"],
+    description="A library for testing Python applications in "
+    "self-contained Kerberos 5 environments",
+    long_description=open("README.md").read(),
+    license="LICENSE.txt",
     url="https://github.com/pythongssapi/k5test",
     python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Security',
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Security",
     ],
-    keywords=['gssapi', 'security'],
+    keywords=["gssapi", "security"],
     install_requires=[],
-    extras_require={
-        'extension_test': ['gssapi']
-    }
+    extras_require={"extension_test": ["gssapi"]},
 )
